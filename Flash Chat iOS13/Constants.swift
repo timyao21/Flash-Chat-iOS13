4fc3503a23abeead911b255d492c0f0c9bb57243
biubiubiupiu21 <94020092+timyao21@users.noreply.github.com>
2024-11-06 13:35:12 -0500

    added swiftpm, setted up firebase. Tested login and register

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Junyu Yao on 11/6/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Junyu Yao on 11/6/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
